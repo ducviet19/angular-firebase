faece54f7700a0d9a7d638f15878a66ff52cd4f5
ducviet19 <54992665+ducviet19@users.noreply.github.com>
2020-10-26 10:52:20 +0700

    first

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCLDfbm5dKwPLtk0Bobk9YTI-zmhcLM0HI",
+    authDomain: "gkz-angular-firebase-bddf8.firebaseapp.com",
+    databaseURL: "https://gkz-angular-firebase-bddf8.firebaseio.com",
+    projectId: "gkz-angular-firebase-bddf8",
+    storageBucket: "gkz-angular-firebase-bddf8.appspot.com",
+    messagingSenderId: "1007671125146",
+    appId: "1:1007671125146:web:2afa38d017e05e321aab23",
+    measurementId: "G-MQ0DZM31JZ"
+  }
+};
